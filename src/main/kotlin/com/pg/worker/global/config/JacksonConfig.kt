42dc07e835ea244fb53d01141c00ea2deadf578f
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2026-03-09 16:46:49 +0900

    fix(settlement): 날짜 응답이 배열로 직렬화되지 않도록 WRITE_DATES_AS_TIMESTAMPS 비활성화

--- src/main/kotlin/com/pg/worker/global/config/JacksonConfig.kt
+++ src/main/kotlin/com/pg/worker/global/config/JacksonConfig.kt
@@ -1,6 +1,7 @@
 package com.pg.worker.global.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
@@ -16,4 +17,5 @@ class JacksonConfig {
         ObjectMapper()
             .registerKotlinModule()
             .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 }
