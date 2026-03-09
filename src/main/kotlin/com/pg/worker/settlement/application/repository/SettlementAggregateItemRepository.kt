9be8e979691727cd9fc90533789eaa2e82a588af
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2026-03-09 12:37:02 +0900

    fix(settlement): PR 반영한 코드 수정

--- src/main/kotlin/com/pg/worker/settlement/application/repository/SettlementAggregateItemRepository.kt
+++ src/main/kotlin/com/pg/worker/settlement/application/repository/SettlementAggregateItemRepository.kt
@@ -3,6 +3,5 @@ package com.pg.worker.settlement.application.repository
 import com.pg.worker.settlement.domain.SettlementAggregateItem
 
 interface SettlementAggregateItemRepository {
-    fun save(item: SettlementAggregateItem): SettlementAggregateItem
     fun saveAll(items: List<SettlementAggregateItem>): List<SettlementAggregateItem>
 }
