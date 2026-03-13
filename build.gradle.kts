3ba146184e7b1034569e8596411d343ab46cb846
sanseongKo <tkstjd56@naver.com>
2026-03-14 03:20:17 +0900

    actuator 추가 및 deploy.yml 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     // --- Kotlin ---
     implementation("org.jetbrains.kotlin:kotlin-reflect")
